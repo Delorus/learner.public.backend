8e0d941744e4e218e34456b15eb6ecc1f9f867e3
mshherbakov <sherbakov.maks@gmail.com>
2019-08-26 21:46:36 +0500

    initial commit

---
+++ src/main/kotlin/ru/sherb/igorprj/IgorprjApplication.kt
@@ -0,0 +1,11 @@
+package ru.sherb.igorprj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IgorprjApplication
+
+fun main(args: Array<String>) {
+    runApplication<IgorprjApplication>(*args)
+}
