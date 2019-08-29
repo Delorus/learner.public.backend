f1b1045ee734fe8b45e30a745727ad6150f4c1ff
mshherbakov <sherbakov.maks@gmail.com>
2019-08-29 21:59:43 +0500

    enable transactional management

--- src/main/kotlin/ru/sherb/igorprj/IgorprjApplication.kt
+++ src/main/kotlin/ru/sherb/igorprj/IgorprjApplication.kt
@@ -2,8 +2,10 @@ package ru.sherb.igorprj
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class IgorprjApplication
 
 fun main(args: Array<String>) {
