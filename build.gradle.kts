631e0094a247ffd8f16c9673d1057957c402a20c
mshherbakov <sherbakov.maks@gmail.com>
2019-08-31 22:59:10 +0500

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.2.71"
-    kotlin("plugin.spring") version "1.2.71"
+    kotlin("jvm") version "1.3.50"
+    kotlin("plugin.spring") version "1.3.50"
 }
 
 group = "ru.sherb"
 version = "0.1.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val kotlinVersion = "1.3.50"
 
 repositories {
     mavenCentral()
@@ -23,8 +25,8 @@ dependencies {
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
     implementation("io.springfox:springfox-swagger2:2.9.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.2.71")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.71")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     runtimeOnly("org.postgresql:postgresql:42.2.6")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 //    testImplementation("org.springframework.security:spring-security-test")
