e3862ac5ce0c87c18fd86e7233863a2edec39bde
mshherbakov <sherbakov.maks@gmail.com>
2019-09-07 21:43:52 +0500

    add authorize button for swagger

--- src/main/kotlin/ru/sherb/igorprj/swagger/config/SwaggerConfig.kt
+++ src/main/kotlin/ru/sherb/igorprj/swagger/config/SwaggerConfig.kt
@@ -2,11 +2,17 @@ package ru.sherb.igorprj.swagger.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.Pageable
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 /**
  * @author maksim
@@ -22,4 +28,24 @@ class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
             .build()
+            .ignoredParameterTypes(Pageable::class.java)
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
+            .useDefaultResponseMessages(false)
+
+    private fun apiKey(): ApiKey {
+        return ApiKey("JWT", "Authorization", "header")
+    }
+
+    private fun securityContext(): SecurityContext {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/v.*"))
+                .build()
+    }
+
+    fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        return listOf(SecurityReference("JWT", arrayOf(authorizationScope)))
+    }
 }
